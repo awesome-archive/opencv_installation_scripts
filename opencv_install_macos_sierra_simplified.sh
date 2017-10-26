40206a0025ee76c53861afa895c7d8f8af61321b
Adam Gradzki <adam@mm.st>
2017-10-26 10:10:18 -0500

    Updated Mac OS X installation script

--- opencv_install_macos_sierra_simplified.sh
+++ opencv_install_macos_sierra_simplified.sh
@@ -5,6 +5,10 @@ set -o nounset
 set -o pipefail
 
 function run () {
+
+OPENCV_VERSION='3.3.1'
+PYTHON_VERSION='3.6.3'
+
 # step 1. install brew (http://brew.sh)
 if ! type "brew" > /dev/null; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -21,11 +25,9 @@ if "pyenv" > /dev/null; then
   exit 1
 fi
 
-# install Python 3.6.2 if it is not installed
-# env PYTHON_CONFIGURE_OPTS="--enable-shared --enable-optimizations" pyenv install 3.6.2
-# pyenv global 3.6.2
+# install Python if it is not installed
 
-if [[ `python --version` != "Python 3.6.2" ]]; then
+if [[ `python --version` != "Python ${PYTHON_VERSION}" ]]; then
   echo "error: python installation failure"
   echo "info: check if pyenv is installed correctly"
   exit 1
@@ -37,11 +39,12 @@ if [[ `which python` != "${HOME}/.pyenv/shims/python" ]]; then
   exit 1
 fi
 
-rm -rf /opt/src/opencv33_py36
-rm -rf /opt/src/opencv33_py36_contrib
+rm -rf "/opt/src/opencv*"
 
-# step 3. install numpy
-pip install -U pip setuptools wheel cython numpy
+# step 3. install python dependencies
+pip install -U pip setuptools
+pip install -U wheel
+pip install -U cython numpy
 
 # step 4. build opencv
 sudo mkdir -p /opt/src
@@ -49,23 +52,26 @@ sudo chown $(whoami):staff /opt
 sudo chown $(whoami):staff /opt/src
 cd /opt/src
 if [[ ! -r "opencv33.zip" ]]; then
-    echo "Cannot find opencv33.zip. Downloading..."
-    curl -L https://github.com/opencv/opencv/archive/3.3.0.zip -o opencv33.zip
+    echo "Cannot find opencv_${OPENCV_VERSION}.zip. Downloading..."
+    curl -L https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip -o opencv_${OPENCV_VERSION}.zip
 fi
 if [[ ! -r "opencv33contrib.zip" ]]; then
-    echo "Cannot find opencv33contrib.zip. Downloading..."
-    curl -L https://github.com/opencv/opencv_contrib/archive/3.3.0.zip -o opencv33contrib.zip
+    echo "Cannot find opencv_contrib_${OPENCV_VERSION}.zip. Downloading..."
+    curl -L https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip -o opencv_contrib_${OPENCV_VERSION}.zip
 fi
-unzip opencv33.zip
-unzip opencv33contrib.zip
-mv -v opencv-3.3.0 /opt/src/opencv33_py36
-mv -v opencv_contrib-3.3.0 /opt/src/opencv33_py36_contrib
-cd /opt/src/opencv33_py36
-mkdir /opt/src/opencv33_py36/release
-cd /opt/src/opencv33_py36/release
-cmake \
-    -D CMAKE_INSTALL_PREFIX=/opt/opencv33_py36 \
-    -D OPENCV_EXTRA_MODULES_PATH=/opt/src/opencv33_py36_contrib/modules \
+unzip opencv_${OPENCV_VERSION}.zip
+unzip opencv_contrib_${OPENCV_VERSION}.zip
+mv -v opencv-${OPENCV_VERSION} /opt/src/opencv_${OPENCV_VERSION}_python_${PYTHON_VERSION}
+mv -v opencv_contrib-${OPENCV_VERSION} /opt/src/opencv_contrib_${OPENCV_VERSION}_python_${PYTHON_VERSION}
+cd /opt/src/opencv_${OPENCV_VERSION}_python_${PYTHON_VERSION}
+mkdir release
+cd release
+
+CFLAGS='-march=native -O2 -pipe' \
+CXXFLAGS='-march=native -O2 -pipe' \
+    cmake \
+    -D CMAKE_INSTALL_PREFIX=/opt/opencv_${OPENCV_VERSION}_python_${PYTHON_VERSION} \
+    -D OPENCV_EXTRA_MODULES_PATH=/opt/src/opencv_contrib_${OPENCV_VERSION}_python_${PYTHON_VERSION}/modules \
     -D BUILD_OPENCV_PYTHON2=OFF \
     -D BUILD_OPENCV_PYTHON3=ON \
     -D BUILD_TIFF=ON \
@@ -78,13 +84,15 @@ cmake \
     -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") ..
     make -j8
     make install
-    # Installs to: ~/.pyenv/versions/3.6.2/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so
+    # Installs to (if using 3.6.x): ~/.pyenv/versions/${PYTHON_VERSION}/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so
     
     ### Example installation for a virtual environment named "demo"
-    # pyenv virtualenv 3.6.2 demo
+    # pyenv virtualenv 3.6.x demo
     # pyenv global demo
     # pip install -U pip setuptools wheel numpy
-    # ln -s "$HOME/.pyenv/versions/3.6.1/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so" \
+    # ln -s "$HOME/.pyenv/versions/3.6.x/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so" \
     #     "$HOME/.pyenv/versions/demo/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so"
 }
+
 run
+
