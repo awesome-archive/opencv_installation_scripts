64b4d3a756e9e5bf5eb2270f0bce2bb07e1330c3
Adam Gradzki <adam@mm.st>
2017-10-31 12:16:49 -0500

    Amazon Linux OpenCV with Eigen3 latest

--- opencv_install_amazon_linux.sh
+++ opencv_install_amazon_linux.sh
@@ -6,21 +6,20 @@ set -o pipefail
 
 function run () {
 
+NPROC=$(nproc)
+EIGEN_VERSION='3.3.4'
 OPENCV_VERSION='3.3.1'
 PYTHON_VERSION='3.6.3'
 
-# step 1. install brew (http://brew.sh)
-if ! type "brew" > /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-brew cleanup || true
-brew update || true
-brew upgrade || true
-brew tap homebrew/science || true
-brew install protobuf eigen openjpeg tbb hdf5 tesseract libjpeg-turbo libtiff libpng pyenv-virtualenv || true
-brew cleanup || true
+sudo yum install -y libpng-devel libjpeg-turbo-devel jasper-devel \
+    libtiff-devel libwebp-devel tbb-devel cmake openblas-devel \
 
-if "pyenv" > /dev/null; then
+find /opt/src -maxdepth 1 -type d -iname "opencv*" -exec rm -rf "{}" \;
+find /opt/src -maxdepth 1 -type d -iname "eigen*" -exec rm -rf "{}" \;
+find /opt/ -maxdepth 1 -type d -iname "opencv*" -exec rm -rf "{}" \;
+find /opt/ -maxdepth 1 -type d -iname "eigen*" -exec rm -rf "{}" \;
+
+if "pyenv" &> /dev/null; then
   echo "error: pyenv not installed properly"
   echo "info: install pyenv!"
   exit 1
@@ -40,8 +39,6 @@ if [[ `which python` != "${HOME}/.pyenv/shims/python" ]]; then
   exit 1
 fi
 
-rm -rf "/opt/src/opencv*"
-
 # step 3. install python dependencies
 pip install -U pip setuptools
 pip install -U wheel
@@ -49,32 +46,47 @@ pip install -U cython numpy
 
 # step 4. build opencv
 sudo mkdir -p /opt/src
-sudo chown $(whoami):staff /opt
-sudo chown $(whoami):staff /opt/src
+sudo chown $(whoami) /opt
+sudo chown $(whoami) /opt/src
 cd /opt/src
+# install eigen 3
+
+if [[ ! -r "eigen_${EIGEN_VERSION}.zip" ]]; then
+    echo "Cannot find eigen_${EIGEN_VERSION}.zip. Downloading..."
+    curl -L "http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.zip" -o "eigen_${EIGEN_VERSION}.zip" 
+fi
+
 if [[ ! -r "opencv_${OPENCV_VERSION}.zip" ]]; then
     echo "Cannot find opencv_${OPENCV_VERSION}.zip. Downloading..."
-    curl -L https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip -o opencv_${OPENCV_VERSION}.zip
+    curl -L "https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip" -o "opencv_${OPENCV_VERSION}.zip"
 fi
 if [[ ! -r "opencv_contrib_${OPENCV_VERSION}.zip" ]]; then
     echo "Cannot find opencv_contrib_${OPENCV_VERSION}.zip. Downloading..."
-    curl -L https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip -o opencv_contrib_${OPENCV_VERSION}.zip
+    curl -L "https://github.com/opencv/opencv_contrib/archive/${OPENCV_VERSION}.zip" -o "opencv_contrib_${OPENCV_VERSION}.zip"
 fi
-unzip opencv_${OPENCV_VERSION}.zip
-unzip opencv_contrib_${OPENCV_VERSION}.zip
+
+unzip eigen_${EIGEN_VERSION}.zip 
+mv -v eigen-eigen* /opt/eigen_${EIGEN_VERSION}
+
+unzip opencv_${OPENCV_VERSION}.zip 
 mv -v opencv-${OPENCV_VERSION} /opt/src/opencv_${OPENCV_VERSION}_python_${PYTHON_VERSION}
+
+unzip opencv_contrib_${OPENCV_VERSION}.zip
 mv -v opencv_contrib-${OPENCV_VERSION} /opt/src/opencv_contrib_${OPENCV_VERSION}_python_${PYTHON_VERSION}
+
 cd /opt/src/opencv_${OPENCV_VERSION}_python_${PYTHON_VERSION}
 mkdir release
 cd release
 
-CFLAGS='-march=native -O2 -pipe' \
-CXXFLAGS='-march=native -O2 -pipe' \
+CFLAGS="-march=native -O2 -pipe" \
+CXXFLAGS="-march=native -O2 -pipe" \
 cmake \
+-D CMAKE_INCLUDE_PATH="/opt/eigen_${EIGEN_VERSION}" \
 -D CMAKE_INSTALL_PREFIX=/opt/opencv_${OPENCV_VERSION}_python_${PYTHON_VERSION} \
 -D OPENCV_EXTRA_MODULES_PATH=/opt/src/opencv_contrib_${OPENCV_VERSION}_python_${PYTHON_VERSION}/modules \
 -D BUILD_OPENCV_PYTHON2=OFF \
 -D BUILD_OPENCV_PYTHON3=ON \
+-D WITH_EIGEN=ON \
 -D BUILD_TIFF=ON \
 -D BUILD_OPENCV_JAVA=OFF \
 -D BUILD_PERF_TESTS=OFF \
@@ -83,8 +95,10 @@ cmake \
 -D PYTHON3_EXECUTABLE=$(which python) \
 -D PYTHON3_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
 -D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") ..
-make -j8
+exit 0
+make -j${NPROC}
 make install
+
 ### If using 3.6.x:
 ### Installs to: ~/.pyenv/versions/${PYTHON_VERSION}/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so
 
