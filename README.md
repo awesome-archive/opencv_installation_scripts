17944670ff6c9cdf51e444e43cbce5a3c061eec2
Adam Gradzki <adam@mm.st>
2017-11-23 14:47:42 -0600

    README.md: better instructions

--- README.md
+++ README.md
@@ -5,9 +5,34 @@ This script WILL use pyenv and build everything from scratch if necessary.
 Deactivate your exiting Pythons or you will run into big trouble unless you know exactly
 what you are doing.
 
-## 1 GB RAM/swap minimum to compile or compiler WILL explode!
+IMPORTANT! Allocate 1 GB combined RAM and swap at the minimum to compile OpenCV or your compiler and machine will explode and catch on fire!
 
-macOS script is up-to-date.
-Amazon Linux script is up-to-date.
+The macOS script is up-to-date!
 
-TODO: update Ubuntu Linux scripts.
+The Amazon Linux script could use some clean ups.
+
+Ubuntu scripts are outdated but easily adapted and should still work fine.
+
+## How do I use multiple virtual environments?
+
+Let's assume you are using Python 3.6.x and you want to use OpenCV in another virtual environment. The default build puts it in the "opencv" virtual environment.
+
+The script defaults installation to `~/.pyenv/versions/${PYTHON_VERSION}/lib/python3.6/site-packages/cv2*` for Python 3.6.x
+
+If you want to use a different virtual environment, create it:
+    
+    pyenv virtualenv 3.6.x demo
+
+Change to it:
+
+    pyenv global demo
+
+Install the same version of numpy you used to build OpenCV. I am assuming you built OpenCV recently, so the newest numpy is still ABI compatible. This is a big assumption! Be aware.
+
+    pip install -U pip setuptools wheel numpy
+
+Make that symbolic link!
+
+    ln -s "$HOME/.pyenv/versions/${PYTHON_VERSION}/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so" \
+          "$HOME/.pyenv/versions/${MY_VIRTUAL_ENVIRONMENT_NAME}/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so"
+
--- opencv_install_macos_high_sierra.sh
+++ opencv_install_macos_high_sierra.sh
@@ -12,6 +12,8 @@ function run () {
 OPENCV_VERSION='3.3.1'
 PYTHON_VERSION='3.6.3'
 
+pyenv global system || true
+
 if [ "$(which python)" != "/usr/bin/python" ]; then
     echo Script must be run using Mac OS X default Python interpreter
     exit 1
@@ -40,36 +42,41 @@ echo "Installing pyenv and pyenv virtualenv plugin"
 brew install pyenv pyenv-virtualenv
 
 echo "Checking for Python ${PYTHON_VERSION}"
-pyenv versions | python -c "import sys; [sys.exit(0) for l in sys.stdin.readlines() if l.startswith('* ${PYTHON_VERSION}')]; sys.exit(1)" && rc=$? || rc=$?
+_CMD="import sys; [sys.exit(0) for l in sys.stdin.readlines() if l[2:].startswith('${PYTHON_VERSION}')]; sys.exit(1)"
+pyenv versions | python -c "${_CMD}" && rc=$? || rc=$?
 
-if [ ${rc} -ne 0 ]; then
+
+if [ ${rc} -eq 0 ]; then
+    echo "Found an existing Python ${PYTHON_VERSION}"
+else
     echo "Building Python ${PYTHON_VERSION} from source"
     CFLAGS="-I$(brew --prefix readline)/include -I$(brew --prefix openssl)/include -march=native" \
     LDFLAGS="-L$(brew --prefix readline)/lib    -L$(brew --prefix openssl)/lib" \
     CONFIGURE_OPTS="--enable-shared --enable-optimizations --with-computed-gotos" \
     MAKE_OPTS="-j8" \
     pyenv install ${PYTHON_VERSION} --verbose
-else
-    echo "Found an existing Python ${PYTHON_VERSION}"
 fi
 
-echo "Attempting to set Python ${PYTHON_VERSION} as the global interpreter"
-pyenv global ${PYTHON_VERSION}
-
 echo "Creating OpenCV virtualenv"
-pyenv virtualenv ${PYTHON_VERSION} opencv --verbose
+pyenv virtualenv ${PYTHON_VERSION} opencv --verbose || true
 
-exit
+pyenv global opencv
+python3 --version
 
-# step 3. install python dependencies
-pip install -U pip setuptools
-pip install -U wheel
-pip install -U cython numpy
+pip3 install -U setuptools
+pip3 install -U pip
+pip3 install -U wheel
+pip3 install -U cython
+pip3 install -U numpy
 
-# step 4. build opencv
+find /opt/src -maxdepth 1 -type d -iname "opencv*" -exec rm -rf "{}" \;
+find /opt/ -maxdepth 1 -type d -iname "opencv*" -exec rm -rf "{}" \;
+
 sudo mkdir -p /opt/src
-sudo chown $(whoami):staff /opt
-sudo chown $(whoami):staff /opt/src
+sudo chown $(whoami) /opt
+sudo chmod 777 /opt
+sudo chown $(whoami) /opt/src
+sudo chmod 777 /opt/src
 cd /opt/src
 if [[ ! -r "opencv_${OPENCV_VERSION}.zip" ]]; then
     echo "Cannot find opencv_${OPENCV_VERSION}.zip. Downloading..."
@@ -92,32 +99,26 @@ CXXFLAGS='-march=native -O2 -pipe' \
 cmake \
 -D CMAKE_INSTALL_PREFIX=/opt/opencv_${OPENCV_VERSION}_python_${PYTHON_VERSION} \
 -D OPENCV_EXTRA_MODULES_PATH=/opt/src/opencv_contrib_${OPENCV_VERSION}_python_${PYTHON_VERSION}/modules \
+-D ENABLE_FAST_MATH=1 \
 -D BUILD_OPENCV_PYTHON2=OFF \
 -D BUILD_OPENCV_PYTHON3=ON \
--D BUILD_TIFF=ON \
 -D BUILD_OPENCV_JAVA=OFF \
 -D BUILD_PERF_TESTS=OFF \
 -D CMAKE_BUILD_TYPE=RELEASE \
--D PYTHON3_LIBRARY=$(python -c "import os.path; from distutils.sysconfig import get_config_var; print(os.path.join(get_config_var('LIBDIR'), get_config_var('LDLIBRARY')))") \
--D PYTHON3_EXECUTABLE=$(which python) \
--D PYTHON3_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
--D PYTHON3_PACKAGES_PATH=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") ..
+-D PYTHON3_LIBRARY=$(python3 -c "import os.path; from distutils.sysconfig import get_config_var; print(os.path.join(get_config_var('LIBDIR'), get_config_var('LDLIBRARY')))") \
+-D PYTHON3_EXECUTABLE=$(which python3) \
+-D PYTHON3_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())") \
+-D PYTHON3_PACKAGES_PATH=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())") ..
 make -j8
 make install
 ### If using 3.6.x:
 ### Installs to: ~/.pyenv/versions/${PYTHON_VERSION}/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so
 
-### Example installation for a virtual environment named "demo"
-### If using 3.6.x:
 # pyenv virtualenv 3.6.x demo
 # pyenv global demo
 # pip install -U pip setuptools wheel numpy
-# ln -s "$HOME/.pyenv/versions/3.6.x/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so" \
+# ln -s "$HOME/.pyenv/versions/${PYTHON_VERSION}/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so" \
 #     "$HOME/.pyenv/versions/demo/lib/python3.6/site-packages/cv2.cpython-36m-darwin.so"
-
-### OPTIONAL: some housekeeping to free up space and old installations:
-# rm -rf /opt/opencv3*
-# rm -rf /opt/src
 
 }
 
